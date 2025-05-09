bab7eda75e142fe08b01d3a05db553497bf56530
Marlon Dinter <m.dinter@simplicity.ag>
2025-05-09 09:59:39 +0200

    make tests

--- test/PlayerTest.php
+++ test/PlayerTest.php
@@ -13,8 +13,11 @@ class PlayerTest extends TestCase
      */
     public function testCanCreateCharacter (string $name, int $age, string $characterClass): void
     {
-        $player = new Player(new CharacterClass($characterClass), $age, $name);
+        $characterMock = $this->createMock(CharacterClass::class);
+        $characterMock->method("getValue")->willReturn(CharacterClass::BARBARIAN);
+        $player = new Player($characterMock, $age, $name);
         $this->assertInstanceOf(Player::class, $player);
+        var_dump($player->getCharacterClass()->getValue());
     }
     public static function providePossibleCharacters (): array
     {
